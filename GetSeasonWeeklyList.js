3f8edb420ea0bfd4a2c219729dadb072a03927b0
MacTaylor <cslag@hotmail.com.tw>
2016-05-20 22:48:56 +0800

    Parser tested!

--- GetSeasonWeeklyList.js
+++ GetSeasonWeeklyList.js
@@ -1,21 +1,28 @@
 var request = require('request');
 
-var regstr = /<script\b[^>]*>([\s\S]*?)<\/script>/gm; // parse javascript.
+module.exports = function(callback){
+	var regstr = /<script\b[^>]*>([\s\S]*?)<\/script>/gm; // parse javascript.
+	request("http://share.dmhy.org/cms/page/name/programme.html",function(err,res,body){
+		if(!err && res.statusCode == 200){
 
-request("http://share.dmhy.org/cms/page/name/programme.html",function(err,res,body){
-	if(!err && res.statusCode == 200){
+			var javascript_regexp_array = [];
 
-		var javascript_regexp_array = [];
+			var match;
+			while(match = regstr.exec(body)){
+				javascript_regexp_array.push(match[1]);
+			}
 
-		var match;
-		while(match = regstr.exec(body)){
-			javascript_regexp_array.push(match[1]);
+			eval(javascript_regexp_array[5]); //active javascript in html.
+			
+			callback({
+				sunday : sunarray,
+				monday : monarray,
+				tuesday : tuearray,
+				wednesday : wedarray,
+				thursday : thuarray,
+				friday : friarray,
+				saturday : satarray
+			})
 		}
-
-		eval(javascript_regexp_array[5]); //active javascript in html.
-
-		for(var i = 0;i<sunarray.length;i++){ // get sunday list
-			console.log(sunarray[i][1]);
-		}
-	}
-});
+	});
+}
---
+++ test.js
@@ -0,0 +1,64 @@
+var weeklyList = require("./GetSeasonWeeklyList.js");
+
+
+new weeklyList(function(weekly){
+	console.log("Monday:")
+	var mondayList = weekly.monday;
+	for(var i = 0;i<mondayList.length;i++){ // get sunday list
+		var name = mondayList[i][1];
+		console.log(name);
+	}
+	console.log("-------------------------------");
+});
+
+
+
+new weeklyList(function(weekly){
+	console.log("Tuesday:")
+	var tuesdayList = weekly.tuesday;
+	for(var i = 0;i<tuesdayList.length;i++){ // get sunday list
+		var name = tuesdayList[i][1];
+		console.log(name);
+	}
+	console.log("-------------------------------");
+});
+
+new weeklyList(function(weekly){
+	console.log("Wednesday:")
+	var wednesdayList = weekly.wednesday;
+	for(var i = 0;i<wednesdayList.length;i++){ // get sunday list
+		var name = wednesdayList[i][1];
+		console.log(name);
+	}
+	console.log("-------------------------------");
+});
+
+new weeklyList(function(weekly){
+	console.log("Thursday:")
+	var thursdayList = weekly.thursday;
+	for(var i = 0;i<thursdayList.length;i++){ // get sunday list
+		var name = thursdayList[i][1];
+		console.log(name);
+	}
+	console.log("-------------------------------");
+});
+
+new weeklyList(function(weekly){
+	console.log("Friday:")
+	var fridayList = weekly.friday;
+	for(var i = 0;i<fridayList.length;i++){ // get sunday list
+		var name = fridayList[i][1];
+		console.log(name);
+	}
+	console.log("-------------------------------");
+});
+
+new weeklyList(function(weekly){
+	console.log("Saturday:")
+	var saturdayList = weekly.saturday;
+	for(var i = 0;i<saturdayList.length;i++){ // get sunday list
+		var name = saturdayList[i][1];
+		console.log(name);
+	}
+	console.log("-------------------------------");
+});
